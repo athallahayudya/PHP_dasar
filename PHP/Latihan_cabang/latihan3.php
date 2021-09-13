e323e20031ca75a2e25b60dec64e5287a9f4b5f5
athallahayudya <74366958+athallahayudya@users.noreply.github.com>
2021-09-13 21:36:48 +0700

    Add files via upload

---
+++ PHP/Latihan_cabang/latihan3.php
@@ -0,0 +1,9 @@
+<?php
+    $user = "Athallah";
+    $pass = 456;
+    if ($user == "Athallah" && $pass == "123"){
+        echo "Login Berhasil";
+    } else {
+        echo "Login Gagal";
+    }
+?>
